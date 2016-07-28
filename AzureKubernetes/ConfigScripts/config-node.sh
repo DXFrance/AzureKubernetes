0231b0e8585e090487802da8668ec9e48c902c66
stephgou <stephgou@microsoft.com>
2016-07-28 14:28:43 +0200

    Update config-node.sh IP and SSH logic

--- AzureKubernetes/ConfigScripts/config-node.sh
+++ AzureKubernetes/ConfigScripts/config-node.sh
@@ -6,7 +6,7 @@ function usage()
     echo "Usage: config-node.sh [storage-account-name] [storage-account-key] [ansible user]"
 }
 
-error_log()
+function error_log()
 {
     if [ "$?" != "0" ]; then
         log "$1"
@@ -18,46 +18,33 @@ error_log()
 function log()
 {
 	
-  mess="$(hostname): $1"
+  x=":ok:"
 
-  logger -t "${BASH_SCRIPT}" "${mess}"
+  if [ "$2" = "x" ]; then
+    x=":question:"
+  fi
 
+  if [ "$2" != "0" ]; then
+    if [ "$2" = "N" ]; then
+       x=""
+    else
+       x=":japanese_goblin:"
+    fi
+  fi
+  mess="$(date) - $(hostname): $1 $x"
+
+  payload="payload={\"icon_emoji\":\":cloud:\",\"text\":\"$mess\"}"
+  curl -s -X POST --data-urlencode "$payload" "$LOG_URL" > /dev/null 2>&1
+    
+  echo "$(date) : $1"
 }
-function ssh_config()
+
+function fix_etc_hosts()
 {
-  log "Configure ssh..."
-  log "Create ssh configuration for ${ANSIBLE_USER}"
-  
-  printf "Host *\n  user %s\n  StrictHostKeyChecking no\n" "${ANSIBLE_USER}"  >> "/home/${ANSIBLE_USER}/.ssh/config"
-  
-  error_log "Unable to create ssh config file for user ${ANSIBLE_USER}"
-  
-  log "Copy generated keys..."
-  
-  cp id_rsa "/home/${ANSIBLE_USER}/.ssh/id_rsa"
-  error_log "Unable to copy id_rsa key to $ANSIBLE_USER .ssh directory"
-
-  cp id_rsa.pub "/home/${ANSIBLE_USER}/.ssh/id_rsa.pub"
-  error_log "Unable to copy id_rsa.pub key to $ANSIBLE_USER .ssh directory"
-  
-  cat "/home/${ANSIBLE_USER}/.ssh/id_rsa.pub" >> "/home/${ANSIBLE_USER}/.ssh/authorized_keys"
-  error_log "Unable to copy $ANSIBLE_USER id_rsa.pub to authorized_keys "
-
-  chmod 700 "/home/${ANSIBLE_USER}/.ssh"
-  error_log "Unable to chmod $ANSIBLE_USER .ssh directory"
-
-  chown -R "${ANSIBLE_USER}:" "/home/${ANSIBLE_USER}/.ssh"
-  error_log "Unable to chown to $ANSIBLE_USER .ssh directory"
-
-  chmod 400 "/home/${ANSIBLE_USER}/.ssh/id_rsa"
-  error_log "Unable to chmod $ANSIBLE_USER id_rsa file"
-
-  chmod 644 "/home/${ANSIBLE_USER}/.ssh/id_rsa.pub"
-  error_log "Unable to chmod $ANSIBLE_USER id_rsa.pub file"
-
-  chmod 400 "/home/${ANSIBLE_USER}/.ssh/authorized_keys"
-  error_log "Unable to chmod $ANSIBLE_USER authorized_keys file"
-  
+	log "Add hostame and ip in hosts file ..."
+	IP=$(ip addr show eth0 | grep inet | grep -v inet6 | awk '{ print $2; }' | sed 's?/.*$??')
+	HOST=$(hostname)
+	echo "${IP}" "${HOST}" >> "${HOST_FILE}"
 }
 
 function install_packages()
@@ -99,40 +86,57 @@ function get_sshkeys()
     log "Install azure storage python module ..."
     pip install azure-storage
 
-    # Push both Private and Public Key
-    log "Push ssh keys to Azure Storage"
-    until python GetSSHFromPrivateStorage.py "${STORAGE_ACCOUNT_NAME}" "${STORAGE_ACCOUNT_KEY}" id_rsa
+    # Pull both Private and Public Key
+    log "Get ssh keys from Azure Storage"
+    until python GetSSHFromPrivateStorage.py "${STORAGE_ACCOUNT_NAME}" "${STORAGE_ACCOUNT_KEY}" idgen_rsa
     do
-        log "Fails to Get id_rsa key trying again ..."
+        log "Fails to Get idgen_rsa key trying again ..."
         sleep 60
         let c=${c}+1
         if [ "${c}" -gt 4 ]; then
-           log "Timeout to get id_rsa key exiting ..."
+           log "Timeout to get idgen_rsa key exiting ..."
            exit 1
         fi
     done
-    python GetSSHFromPrivateStorage.py "${STORAGE_ACCOUNT_NAME}" "${STORAGE_ACCOUNT_KEY}" id_rsa.pub
-    error_log "Fails to Get id_rsa.pub key"
+    python GetSSHFromPrivateStorage.py "${STORAGE_ACCOUNT_NAME}" "${STORAGE_ACCOUNT_KEY}" idgen_rsa.pub
+    error_log "Fails to Get idgen_rsa.pub key"
 }
 
-function fix_etc_hosts()
+function ssh_config()
 {
-	log "Add hostame and ip in hosts file ..."
-	IP=$(ip addr show eth0 | grep inet | grep -v inet6 | awk '{ print $2; }' | sed 's?/.*$??')
-	HOST=$(hostname)
-	echo "${IP}" "${HOST}" >> "${HOST_FILE}"
-}
+  log "Configure ssh..."
+  log "Create ssh configuration for ${ANSIBLE_USER}"
+  
+  printf "Host *\n  user %s\n  StrictHostKeyChecking no\n" "${ANSIBLE_USER}"  >> "/home/${ANSIBLE_USER}/.ssh/config"
+  
+  error_log "Unable to create ssh config file for user ${ANSIBLE_USER}"
+  
+  log "Copy generated keys..."
+  
+  cp id_rsa "/home/${ANSIBLE_USER}/.ssh/idgen_rsa"
+  error_log "Unable to copy idgen_rsa key to $ANSIBLE_USER .ssh directory"
 
-function privateIP_for_ansible()
-{
-	FACTS=/etc/ansible/facts
+  cp id_rsa.pub "/home/${ANSIBLE_USER}/.ssh/idgen_rsa.pub"
+  error_log "Unable to copy idgen_rsa.pub key to $ANSIBLE_USER .ssh directory"
+  
+  cat "/home/${ANSIBLE_USER}/.ssh/idgen_rsa.pub" >> "/home/${ANSIBLE_USER}/.ssh/authorized_keys"
+  error_log "Unable to copy $ANSIBLE_USER idgen_rsa.pub to authorized_keys "
 
-	mkdir -p $FACTS
-	echo "${privateIP},${role}" > $FACTS/private-ip-role.fact 
+  chmod 700 "/home/${ANSIBLE_USER}/.ssh"
+  error_log "Unable to chmod $ANSIBLE_USER .ssh directory"
 
-	chmod 755 /etc/ansible
-	chmod 755 /etc/ansible/facts
-	chmod a+r $FACTS/private-ip.fact 
+  chown -R "${ANSIBLE_USER}:" "/home/${ANSIBLE_USER}/.ssh"
+  error_log "Unable to chown to $ANSIBLE_USER .ssh directory"
+
+  chmod 400 "/home/${ANSIBLE_USER}/.ssh/idgen_rsa"
+  error_log "Unable to chmod $ANSIBLE_USER idgen_rsa file"
+
+  chmod 644 "/home/${ANSIBLE_USER}/.ssh/idgen_rsa.pub"
+  error_log "Unable to chmod $ANSIBLE_USER idgen_rsa.pub file"
+
+  chmod 400 "/home/${ANSIBLE_USER}/.ssh/authorized_keys"
+  error_log "Unable to chmod $ANSIBLE_USER authorized_keys file"
+  
 }
 
 function start_nc()
@@ -150,12 +154,25 @@ CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 log "CustomScript Directory is ${CWD}"
 
 BASH_SCRIPT="${0}"
-privateIP="${1}"
-role="${2}"
+STORAGE_ACCOUNT_NAME="${1}"
+STORAGE_ACCOUNT_KEY="${2}"
+ANSIBLE_USER="${3}"
 
-STORAGE_ACCOUNT_NAME="${3}"
-STORAGE_ACCOUNT_KEY="${4}"
-ANSIBLE_USER="${5}"
+LOG_DATE=$(date +%s)
+
+GIT_KUB8_URL="https://github.com/DXFrance/AzureKubernetes.git"
+
+LOG_URL="https://hooks.slack.com/services/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"
+
+# Slack notification
+SLACK_TOKEN="$(get_slack_token)"
+SLACK_CHANNEL="clusternodes"
+
+export SLACK_TOKEN SLACK_CHANNEL
+
+## Repos Variables
+repo_name="ansible-kubernetes-centos"
+slack_repo="slack-ansible-plugin"
 
 ## Call functions
 fix_etc_hosts
@@ -167,6 +184,6 @@ privateIP_for_ansible
 # Script Wait for the wait_module from ansible playbook
 start_nc
 
-log "Success : End of Execution of Install Script from CustomScript"
+log "Success : End of Execution of Install Script from config-node CustomScript"
 
 exit 0
